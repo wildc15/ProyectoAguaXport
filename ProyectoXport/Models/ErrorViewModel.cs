677a0334e567d09badbce87e15eb89a1ac7e25e3
wildc15 <114968381+wildc15@users.noreply.github.com>
2024-01-31 18:40:54 -0400

    Delete ProyectoXport directory

--- ProyectoXport/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoXport.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
